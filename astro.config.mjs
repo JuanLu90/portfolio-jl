3688ea51ad5d2c0862eddf1da12676ab27c6e048
Juan Luis Moreno Calderon <juanlu.moreno@entelgy.com>
2025-08-12 12:10:42 +0200

    refactor(seo): seo, accesibility and semantic updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://www.juanlumoreno.dev",
+  integrations: [sitemap(), robotsTxt()],
+});
